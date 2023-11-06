22aafa96badc19cefc0317e0ed5e6a30dfdc030b
seifmegahed <seifmegahed@me.com>
2023-11-06 03:58:13 +0200

    Three canvas seperate file

---
+++ src/Three/ThreeCanvas.tsx
@@ -0,0 +1,39 @@
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import { Model3 } from "./Pedal3";
+
+const ThreeCanvas = ({ knobs }: { knobs: number[] }) => {
+  // const cameraControlRef = useRef<CameraControls | null>(null);
+  return (
+    <Canvas
+      shadows
+      camera={{ fov: 45, position: [0, 0, 20] }}
+      onCreated={({ camera, gl }) => {
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+      }}
+    >
+      {/* <CameraControls ref={cameraControlRef} /> */}
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        color="white"
+        // castShadow
+        intensity={5}
+        position={[-115, 25, 0]}
+        lookAt={() => new THREE.Vector3(0, -9, 0)}
+      />
+      <Model3
+        knobs={knobs}
+        rotation={[Math.PI * 0.5, 0, Math.PI * 0.3]}
+        position={[10, 0, 0]}
+      />
+      <mesh receiveShadow position={[0, -9, 0]}>
+        <boxGeometry args={[100, 5, 50]} />
+        <meshStandardMaterial color={new THREE.Color("pink")} />
+      </mesh>
+    </Canvas>
+  );
+};
+
+export default ThreeCanvas;
